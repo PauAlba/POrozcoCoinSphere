04e38a6668eb693a366648d324efa59063b1359a
Paulina <paulinaorozcolsb@gmail.com>
2025-09-13 20:14:15 -0600

    Implementar la lista, usar coil 3

--- app/src/main/java/com/example/coinmarketcap/MainActivity.kt
+++ app/src/main/java/com/example/coinmarketcap/MainActivity.kt
@@ -1,25 +1,12 @@
 package com.example.coinmarketcap
 
-//import android.os.Bundle
-//import androidx.activity.ComponentActivity
-//import androidx.activity.compose.setContent
-//import androidx.activity.enableEdgeToEdge
-//import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.foundation.layout.padding
-//import androidx.compose.material3.Scaffold
-//import androidx.compose.material3.Text
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.tooling.preview.Preview
-
-import com.example.coinmarketcap.ui.theme.CoinMarketCapTheme
-
-
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -29,12 +16,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-//import coil.compose.rememberAsyncImagePainter
-
+import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePainter
+import coil3.request.ImageRequest
+import com.example.coinmarketcap.ui.theme.CoinMarketCapTheme
 
 val Background = Color(0xFF0B1020)
 val Surface = Color(0xFF151B2E)
@@ -42,6 +34,12 @@ val TextMain = Color(0xFFE8ECF8)
 val TextDim = Color(0xFF9AA3B2)
 
 
+data class CryptoItem(
+    val id: Int,
+    val name: String,
+    val price: String,
+    val imageUrl: String
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,7 +55,18 @@ class MainActivity : ComponentActivity() {
 // Main
 @Composable
 fun HomeScreen() {
-
+    // datos para los items del lazy column
+    val cryptoList = listOf(
+        CryptoItem(1, "Bitcoin", "$109,797.37", "https://assets.coingecko.com/coins/images/1/large/bitcoin.png"),
+        CryptoItem(2, "Ethereum", "$4,321.21", "https://assets.coingecko.com/coins/images/279/large/ethereum.png"),
+        CryptoItem(3, "Tether", "$1.0000", "https://assets.coingecko.com/coins/images/325/large/Tether-logo.png"),
+        CryptoItem(4, "XRP", "$2.8100", "https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png"),
+        CryptoItem(5, "BNB", "$845.0000", "https://assets.coingecko.com/coins/images/825/large/binance-coin-logo.png"),
+        CryptoItem(6, "Solana", "$201.8500", "https://assets.coingecko.com/coins/images/4128/large/solana.png"),
+        CryptoItem(7, "USDC", "$0.9998", "https://assets.coingecko.com/coins/images/6319/large/USD_Coin_icon.png"),
+        CryptoItem(8, "Dogecoin", "$0.1320", "https://assets.coingecko.com/coins/images/5/large/dogecoin.png"),
+        CryptoItem(9, "TRON", "$0.3630", "https://assets.coingecko.com/coins/images/1094/large/tron-logo.png")
+    )
 
 
     Surface(
@@ -70,7 +79,7 @@ fun HomeScreen() {
                 .padding(horizontal = 16.dp, vertical = 24.dp),
             horizontalAlignment = Alignment.Start
         ) {
-            // Header with title
+            // Header
             Text(
                 text = "CoinSphere",
                 color = TextMain,
@@ -79,14 +88,14 @@ fun HomeScreen() {
                 modifier = Modifier.padding(bottom = 16.dp)
             )
 
-            // Cards
+            //Cards
             InfoCard(title = "Global Market Cap", value = "$2.18T")
             InfoCard(title = "Fear & Greed", value = "Neutral (54)")
             InfoCard(title = "Altcoin Season", value = "No")
 
             Spacer(modifier = Modifier.height(24.dp))
 
-            // List Header
+            //List Header
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -104,7 +113,9 @@ fun HomeScreen() {
                 modifier = Modifier.fillMaxWidth(),
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-
+                items(cryptoList) { crypto ->
+                    CryptoItemRow(crypto = crypto)
+                }
             }
         }
     }
@@ -127,7 +138,53 @@ fun InfoCard(title: String, value: String) {
     }
 }
 
+//item
+@Composable
+fun CryptoItemRow(crypto: CryptoItem) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(MaterialTheme.shapes.medium)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            modifier = Modifier.weight(1f),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text(
+                text = crypto.id.toString(),
+                color = TextDim,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp
+            )
 
+            AsyncImage(
+                model = crypto.imageUrl,
+                contentDescription = "${crypto.name} logo",
+                modifier = Modifier.size(36.dp),
+                contentScale = ContentScale.Fit,
+                placeholder = painterResource(id = R.drawable.ic_launcher_foreground),
+                error = painterResource(id = R.drawable.ic_launcher_foreground)
+            )
+
+            Text(
+                text = crypto.name,
+                color = TextMain,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 18.sp
+            )
+        }
+        Text(
+            text = crypto.price,
+            color = TextMain,
+            fontWeight = FontWeight.Medium,
+            fontSize = 18.sp,
+            modifier = Modifier.wrapContentWidth(Alignment.End)
+        )
+    }
+}
 
 
 @Preview(showBackground = true)
